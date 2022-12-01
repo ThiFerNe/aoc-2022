8e57d6f16a427d7ad2857c12896169ecafb570be
ThiFerNe <thimo.f.neumann@gmail.com>
2022-12-01 13:53:54 +0100

    Add solution for day 1

---
+++ README.md
@@ -0,0 +1,26 @@
+# aoc-2022
+
+[https://github.com/ThiFerNe/aoc-2022](https://github.com/ThiFerNe/aoc-2022)
+
+By ThiFerNe
+
+With aoc-2022 you can see my solutions to the [Advent of Code 2022](https://adventofcode.com/2022).
+
+You are free to copy, modify, and distribute aoc-2022 with attribution under the terms of the MIT License.
+See the `LICENSE` file for details.
+
+## Prerequisites
+
+Before running aoc-2022 you need:
+- [Rust](https://www.rust-lang.org/) with [rustup](https://rustup.rs/)
+
+## Using the project
+
+Each day is its own binary and can be run like `cargo run --bin day01`.
+
+For the full speed use `--release` after `cargo run` like `cargo run --release --bin day01`.
+
+---
+
+*README.md created with the help of [https://github.com/ddbeck/readme-checklist/blob/main/checklist.md](https://github.com/ddbeck/readme-checklist/blob/main/checklist.md).*
+
---
+++ src/day01.rs
@@ -0,0 +1,32 @@
+use std::str::FromStr;
+
+const INPUT: &str = include_str!("../inputs/day01.input");
+
+fn main() -> anyhow::Result<()> {
+    // PART 1 - 9 minutes 10 seconds
+    let mut calories_per_elf = INPUT
+        .split("\n\n")
+        .map(|elves_calories| elves_calories
+            .lines()
+            .map(u64::from_str)
+            .collect::<Result<Vec<_>, _>>())
+        .collect::<Result<Vec<Vec<_>>, _>>()?
+        .into_iter()
+        .map(|elves_calories| elves_calories.iter().sum::<u64>())
+        .collect::<Vec<_>>();
+
+    let calories_of_elf_with_maximum = calories_per_elf
+        .iter()
+        .max()
+        .expect("No elf found");
+    println!("calories_of_elf_with_maximum: {calories_of_elf_with_maximum}");
+
+    // PART 2 - 3 minutes 56 seconds
+    calories_per_elf.sort();
+    let calories_of_top_three_elves = calories_per_elf[calories_per_elf.len() - 1] +
+        calories_per_elf[calories_per_elf.len() - 2] +
+        calories_per_elf[calories_per_elf.len() - 3];
+    println!("calories_of_top_three_elves: {calories_of_top_three_elves}");
+
+    Ok(())
+}
