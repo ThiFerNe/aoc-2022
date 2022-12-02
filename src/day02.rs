03915ded9a6fdbf0f2a215df06fd9255f453ea8a
ThiFerNe <thimo.f.neumann@gmail.com>
2022-12-02 09:59:57 +0100

    Refactor day 2 after new ideas

--- src/day02.rs
+++ src/day02.rs
@@ -4,144 +4,65 @@ const INPUT: &str = include_str!("../inputs/day02.input");
 
 fn main() -> anyhow::Result<()> {
     // PART 1 - 20 minutes 45 seconds
-    let own_total_score_1 = INPUT
-        .lines()
-        .map(RockPaperScissorsRound::from_str)
-        .collect::<Result<Vec<_>, _>>()?
-        .into_iter()
-        .map(|round| u64::from(round.score()))
-        .sum::<u64>();
-    println!("own_total_score_1: {own_total_score_1}");
+    let score_by_guesswork = RockPaperScissorsMatch::from_guess(INPUT)?.score()?;
+    println!("score_by_guesswork: {score_by_guesswork}");
 
     // PART 2 - 11 minutes 2 seconds
-    let own_total_score_2 = INPUT
-        .lines()
-        .map(RockPaperScissorsElfProposition::from_str)
-        .collect::<Result<Vec<_>, _>>()?
-        .into_iter()
-        .map(RockPaperScissorsRound::from)
-        .map(|round| u64::from(round.score()))
-        .sum::<u64>();
-    println!("own_total_score_2: {own_total_score_2}");
+    let score_by_elf_explanation = RockPaperScissorsMatch::from_elf_information(INPUT)?.score()?;
+    println!("score_by_elf_explanation: {score_by_elf_explanation}");
 
     Ok(())
 }
 
-#[derive(Debug, Copy, Clone)]
-enum RockPaperScissorsSign {
-    Rock,
-    Paper,
-    Scissors,
+struct RockPaperScissorsMatch {
+    rounds: Vec<RockPaperScissorsRound>,
 }
 
-impl RockPaperScissorsSign {
-    fn selection_score(self) -> u8 {
-        match self {
-            RockPaperScissorsSign::Rock => 1,
-            RockPaperScissorsSign::Paper => 2,
-            RockPaperScissorsSign::Scissors => 3,
-        }
-    }
-}
-
-impl FromStr for RockPaperScissorsSign {
-    type Err = anyhow::Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "A" | "X" => Ok(Self::Rock),
-            "B" | "Y" => Ok(Self::Paper),
-            "C" | "Z" => Ok(Self::Scissors),
-            _ => Err(anyhow::anyhow!("Unexpected string \"{s}\" found")),
-        }
-    }
-}
-
-#[derive(Debug, Copy, Clone)]
-struct RockPaperScissorsRound {
-    enemy_selection: RockPaperScissorsSign,
-    own_selection: RockPaperScissorsSign,
-}
-
-impl RockPaperScissorsRound {
-    fn winner(self) -> RockPaperScissorsWinner {
-        use RockPaperScissorsSign::{Paper, Rock, Scissors};
-        match (self.own_selection, self.enemy_selection) {
-            (Rock, Rock) | (Paper, Paper) | (Scissors, Scissors) => RockPaperScissorsWinner::Draw,
-            (Rock, Scissors) | (Scissors, Paper) | (Paper, Rock) => RockPaperScissorsWinner::Myself,
-            (Rock, Paper) | (Paper, Scissors) | (Scissors, Rock) => RockPaperScissorsWinner::Enemy,
-        }
-    }
-
-    fn score(self) -> u8 {
-        self.own_selection
-            .selection_score()
-            .saturating_add(self.winner().score())
-    }
-}
-
-impl FromStr for RockPaperScissorsRound {
-    type Err = anyhow::Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let selections: [RockPaperScissorsSign; 2] = s
-            .split(' ')
-            .map(RockPaperScissorsSign::from_str)
-            .collect::<Result<Vec<_>, _>>()?
-            .try_into()
-            .map_err(|a: Vec<RockPaperScissorsSign>| {
-                anyhow::anyhow!(
-                    "Failed to convert Vec with len {} to array with len 2.",
-                    a.len()
-                )
-            })?;
+impl RockPaperScissorsMatch {
+    fn from_guess(input: &str) -> Result<Self, anyhow::Error> {
         Ok(Self {
-            enemy_selection: selections[0],
-            own_selection: selections[1],
+            rounds: input
+                .lines()
+                .map(RockPaperScissorsRound::from_str)
+                .collect::<Result<Vec<_>, _>>()?,
         })
     }
-}
 
-impl From<RockPaperScissorsElfProposition> for RockPaperScissorsRound {
-    fn from(value: RockPaperScissorsElfProposition) -> Self {
-        use RockPaperScissorsSign::{Paper, Rock, Scissors};
-        use RockPaperScissorsWinner::{Draw, Enemy, Myself};
-        let own_selection = match (value.enemy_selection, value.target_winner) {
-            (Rock, Myself) | (Scissors, Enemy) | (Paper, Draw) => Paper,
-            (Paper, Myself) | (Rock, Enemy) | (Scissors, Draw) => Scissors,
-            (Scissors, Myself) | (Paper, Enemy) | (Rock, Draw) => Rock,
-        };
-        Self {
-            enemy_selection: value.enemy_selection,
-            own_selection,
-        }
+    fn from_elf_information(input: &str) -> Result<Self, anyhow::Error> {
+        Ok(Self {
+            rounds: input
+                .lines()
+                .map(|line| {
+                    RockPaperScissorsElfExplanation::from_str(line)
+                        .map(RockPaperScissorsRound::from)
+                })
+                .collect::<Result<Vec<_>, _>>()?,
+        })
+    }
+
+    fn score(&self) -> Result<u64, anyhow::Error> {
+        Ok(self
+            .rounds
+            .iter()
+            .map(|round| {
+                round
+                    .score()
+                    .map(u64::from)
+                    .ok_or_else(|| anyhow::anyhow!("Could not calculate round score."))
+            })
+            .collect::<Result<Vec<_>, _>>()?
+            .into_iter()
+            .sum::<u64>())
     }
 }
 
 #[derive(Debug, Copy, Clone)]
-enum RockPaperScissorsWinner {
-    Enemy,
-    Draw,
-    Myself,
-}
-
-impl RockPaperScissorsWinner {
-    fn score(self) -> u8 {
-        match self {
-            Self::Enemy => 0,
-            Self::Draw => 3,
-            Self::Myself => 6,
-        }
-    }
-}
-
-#[derive(Debug, Copy, Clone)]
-struct RockPaperScissorsElfProposition {
+struct RockPaperScissorsElfExplanation {
     enemy_selection: RockPaperScissorsSign,
     target_winner: RockPaperScissorsWinner,
 }
 
-impl FromStr for RockPaperScissorsElfProposition {
+impl FromStr for RockPaperScissorsElfExplanation {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -168,5 +89,133 @@ impl FromStr for RockPaperScissorsElfProposition {
                 }
             },
         })
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+struct RockPaperScissorsRound {
+    enemy_selection: RockPaperScissorsSign,
+    own_selection: RockPaperScissorsSign,
+}
+
+impl RockPaperScissorsRound {
+    fn winner(self) -> RockPaperScissorsWinner {
+        use RockPaperScissorsSign::{Paper, Rock, Scissors};
+        use RockPaperScissorsWinner::{Draw, Enemy, Myself};
+        match self.own_selection {
+            Rock => match self.enemy_selection {
+                Rock => Draw,
+                Paper => Enemy,
+                Scissors => Myself,
+            },
+            Paper => match self.enemy_selection {
+                Rock => Myself,
+                Paper => Draw,
+                Scissors => Enemy,
+            },
+            Scissors => match self.enemy_selection {
+                Rock => Enemy,
+                Paper => Myself,
+                Scissors => Draw,
+            },
+        }
+    }
+
+    fn score(self) -> Option<u8> {
+        self.own_selection
+            .selection_score()
+            .checked_add(self.winner().score())
+    }
+}
+
+impl FromStr for RockPaperScissorsRound {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let selections: [RockPaperScissorsSign; 2] = s
+            .split(' ')
+            .map(RockPaperScissorsSign::from_str)
+            .collect::<Result<Vec<_>, _>>()?
+            .try_into()
+            .map_err(|vec: Vec<_>| {
+                anyhow::anyhow!(
+                    "Failed to convert Vec with len {} to array with len 2.",
+                    vec.len()
+                )
+            })?;
+        Ok(Self {
+            enemy_selection: selections[0],
+            own_selection: selections[1],
+        })
+    }
+}
+
+impl From<RockPaperScissorsElfExplanation> for RockPaperScissorsRound {
+    fn from(value: RockPaperScissorsElfExplanation) -> Self {
+        use RockPaperScissorsSign::{Paper, Rock, Scissors};
+        use RockPaperScissorsWinner::{Draw, Enemy, Myself};
+        Self {
+            enemy_selection: value.enemy_selection,
+            own_selection: match value.target_winner {
+                Draw => value.enemy_selection,
+                Myself => match value.enemy_selection {
+                    Rock => Paper,
+                    Paper => Scissors,
+                    Scissors => Rock,
+                },
+                Enemy => match value.enemy_selection {
+                    Rock => Scissors,
+                    Paper => Rock,
+                    Scissors => Paper,
+                },
+            },
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+enum RockPaperScissorsSign {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl RockPaperScissorsSign {
+    fn selection_score(self) -> u8 {
+        match self {
+            Self::Rock => 1,
+            Self::Paper => 2,
+            Self::Scissors => 3,
+        }
+    }
+}
+
+impl FromStr for RockPaperScissorsSign {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "A" | "X" => Ok(Self::Rock),
+            "B" | "Y" => Ok(Self::Paper),
+            "C" | "Z" => Ok(Self::Scissors),
+            _ => Err(anyhow::anyhow!("Unexpected string \"{s}\" found")),
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+enum RockPaperScissorsWinner {
+    Enemy,
+    Draw,
+    Myself,
+}
+
+impl RockPaperScissorsWinner {
+    fn score(self) -> u8 {
+        match self {
+            Self::Enemy => 0,
+            Self::Draw => 3,
+            Self::Myself => 6,
+        }
     }
 }
