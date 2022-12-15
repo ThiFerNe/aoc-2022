afb84a8aa8a016168f11bb506e7274be30a399bf
ThiFerNe <thimo.f.neumann@gmail.com>
2022-12-15 12:54:11 +0100

    Apply all pedantic lints to day 10

--- src/day10.rs
+++ src/day10.rs
@@ -33,15 +33,15 @@ fn main() -> anyhow::Result<()> {
 struct CommunicationDevice<const CRT_COLUMNS: usize = 40, const CRT_ROWS: usize = 6> {
     default_program: Option<Program>,
     clock_circuit: ClockCircuit,
-    crt: Rc<RefCell<CRT<CRT_COLUMNS, CRT_ROWS>>>,
+    crt: Rc<RefCell<Crt<CRT_COLUMNS, CRT_ROWS>>>,
 }
 
 impl<const CRT_COLUMNS: usize, const CRT_ROWS: usize> CommunicationDevice<CRT_COLUMNS, CRT_ROWS> {
     fn new() -> Self {
         Self {
             default_program: None,
-            clock_circuit: ClockCircuit::new(CPU::new_with_register_x_value(0)),
-            crt: Rc::new(RefCell::new(CRT::default())),
+            clock_circuit: ClockCircuit::new(Cpu::new_with_register_x_value(0)),
+            crt: Rc::new(RefCell::new(Crt::default())),
         }
     }
 
@@ -60,7 +60,7 @@ impl<const CRT_COLUMNS: usize, const CRT_ROWS: usize> CommunicationDevice<CRT_CO
         look_during_cycles: Vec<u128>,
     ) -> anyhow::Result<i64> {
         self.reset();
-        if let Some(program) = &self.default_program {
+        if let Some(ref program) = self.default_program {
             self.clock_circuit.cpu.load(program.clone());
         }
         self.clock_circuit.cpu.x_register.value = starting_cpu_x_register_value;
@@ -91,7 +91,7 @@ impl<const CRT_COLUMNS: usize, const CRT_ROWS: usize> CommunicationDevice<CRT_CO
         starting_cpu_x_register_value: i64,
     ) -> anyhow::Result<String> {
         self.reset();
-        if let Some(program) = &self.default_program {
+        if let Some(ref program) = self.default_program {
             self.clock_circuit.cpu.load(program.clone());
         }
         self.clock_circuit.cpu.x_register.value = starting_cpu_x_register_value;
@@ -100,7 +100,6 @@ impl<const CRT_COLUMNS: usize, const CRT_ROWS: usize> CommunicationDevice<CRT_CO
 
         self.clock_circuit
             .set_during_cycle_callback(move |cpu, during_cycle| {
-                println!("#{during_cycle} = {cpu:?}");
                 RefCell::borrow_mut(&crt_clone)
                     .process_signal(cpu.x_register.value, during_cycle)
                     .with_context(|| {
@@ -155,9 +154,7 @@ impl FromStr for Instruction {
             Ok(Self::NoOp)
         } else if let Some(value) = lowercase.strip_prefix("addx ") {
             Ok(Self::AddX(
-                value
-                    .parse()
-                    .with_context(|| format!("while parsing AddX value."))?,
+                value.parse().context("while parsing AddX value.")?,
             ))
         } else {
             Err(anyhow::anyhow!("Do not recognize instruction."))
@@ -166,13 +163,13 @@ impl FromStr for Instruction {
 }
 
 #[derive(Debug)]
-struct CPU {
+struct Cpu {
     x_register: Register,
     instruction_register: Option<(Instruction, u128)>,
     loaded_instructions: Option<VecDeque<Instruction>>,
 }
 
-impl CPU {
+impl Cpu {
     fn new_with_register_x_value(value: i64) -> Self {
         Self {
             x_register: Register::new(value),
@@ -191,7 +188,7 @@ impl CPU {
         self.loaded_instructions = Some(program.instructions);
     }
 
-    fn calculate_needed_cycles(instruction: &Instruction) -> u128 {
+    fn calculate_needed_cycles(instruction: Instruction) -> u128 {
         match instruction {
             Instruction::NoOp => 1,
             Instruction::AddX(_) => 2,
@@ -203,8 +200,7 @@ impl CPU {
             || self
                 .loaded_instructions
                 .as_ref()
-                .map(|loaded_instructions| !loaded_instructions.is_empty())
-                .unwrap_or(false)
+                .map_or(false, |loaded_instructions| !loaded_instructions.is_empty())
     }
 
     fn tick(&mut self) -> Result<(), CPUTickError> {
@@ -216,13 +212,19 @@ impl CPU {
                     .ok_or(CPUTickError::NoProgramLoaded)?
                     .pop_front()
                     .ok_or(CPUTickError::ProgramEnded)?;
-                let needed_cycles = Self::calculate_needed_cycles(&next_instruction);
+                let needed_cycles = Self::calculate_needed_cycles(next_instruction);
                 (next_instruction, needed_cycles)
             }
             Some(stored_instruction) => stored_instruction,
         };
         if remaining_cycles > 1 {
-            self.instruction_register = Some((current_instruction, remaining_cycles - 1));
+            let new_remaining_cycles = remaining_cycles.checked_sub(1).ok_or_else(|| {
+                CPUTickError::Internal(
+                    "Could no reduce remaining cycles, which is probably an internal error."
+                        .to_owned(),
+                )
+            })?;
+            self.instruction_register = Some((current_instruction, new_remaining_cycles));
             Ok(())
         } else {
             match current_instruction {
@@ -248,6 +250,8 @@ enum CPUTickError {
     ProgramEnded,
     #[error("Overflow happened.")]
     Overflow,
+    #[error("An internal error occurred ({0}).")]
+    Internal(String),
 }
 
 #[derive(Debug)]
@@ -261,15 +265,17 @@ impl Register {
     }
 }
 
+type CycleCallback = Box<dyn FnMut(&Cpu, u128) -> anyhow::Result<()>>;
+
 struct ClockCircuit {
     cycles_completed: u128,
-    cpu: CPU,
-    cycle_completed_callback: Option<Box<dyn FnMut(&CPU, u128) -> anyhow::Result<()>>>,
-    during_cycle_callback: Option<Box<dyn FnMut(&CPU, u128) -> anyhow::Result<()>>>,
+    cpu: Cpu,
+    cycle_completed_callback: Option<CycleCallback>,
+    during_cycle_callback: Option<CycleCallback>,
 }
 
 impl ClockCircuit {
-    fn new(cpu: CPU) -> Self {
+    fn new(cpu: Cpu) -> Self {
         Self {
             cycles_completed: 0,
             cpu,
@@ -286,23 +292,27 @@ impl ClockCircuit {
     #[allow(dead_code)]
     fn set_cycle_completed_callback<F>(&mut self, cycle_completed_callback: F)
     where
-        F: FnMut(&CPU, u128) -> anyhow::Result<()> + 'static,
+        F: FnMut(&Cpu, u128) -> anyhow::Result<()> + 'static,
     {
         self.cycle_completed_callback = Some(Box::new(cycle_completed_callback));
     }
 
     fn set_during_cycle_callback<F>(&mut self, during_cycle_callback: F)
     where
-        F: FnMut(&CPU, u128) -> anyhow::Result<()> + 'static,
+        F: FnMut(&Cpu, u128) -> anyhow::Result<()> + 'static,
     {
         self.during_cycle_callback = Some(Box::new(during_cycle_callback));
     }
 
     fn run(&mut self) -> anyhow::Result<()> {
         loop {
+            let next_cycles_completed = self
+                .cycles_completed
+                .checked_add(1)
+                .ok_or_else(|| anyhow::anyhow!("Reached maximum cycle count."))?;
             if self.cpu.has_instruction_left() {
-                if let Some(during_cycle_callback) = &mut self.during_cycle_callback {
-                    (during_cycle_callback)(&self.cpu, self.cycles_completed + 1)
+                if let Some(ref mut during_cycle_callback) = self.during_cycle_callback {
+                    (during_cycle_callback)(&self.cpu, next_cycles_completed)
                         .context("while calling during_cycle_callback")?;
                 }
             }
@@ -311,8 +321,8 @@ impl ClockCircuit {
                 Err(CPUTickError::ProgramEnded) => break Ok(()),
                 other => other.with_context(|| format!("after cycle {}", self.cycles_completed))?,
             }
-            self.cycles_completed += 1;
-            if let Some(cycle_completed_callback) = &mut self.cycle_completed_callback {
+            self.cycles_completed = next_cycles_completed;
+            if let Some(ref mut cycle_completed_callback) = self.cycle_completed_callback {
                 (cycle_completed_callback)(&self.cpu, self.cycles_completed)
                     .context("while calling cycle_completed_callback")?;
             }
@@ -321,39 +331,66 @@ impl ClockCircuit {
 }
 
 #[derive(Debug, Eq, PartialEq, Clone)]
-struct CRT<const COLUMNS: usize = 40, const ROWS: usize = 6> {
+struct Crt<const COLUMNS: usize = 40, const ROWS: usize = 6> {
     buffer: [[Pixel; COLUMNS]; ROWS],
 }
 
-impl<const COLUMNS: usize, const ROWS: usize> CRT<COLUMNS, ROWS> {
+impl<const COLUMNS: usize, const ROWS: usize> Crt<COLUMNS, ROWS> {
     fn reset(&mut self) {
         self.buffer.iter_mut().for_each(|buffer_line| {
             buffer_line
                 .iter_mut()
-                .for_each(|pixel| *pixel = Pixel::Dark)
+                .for_each(|pixel| *pixel = Pixel::Dark);
         });
     }
 
     fn process_signal(&mut self, signal: i64, during_cycle: u128) -> anyhow::Result<()> {
-        let during_cycle = during_cycle as usize;
-        let row = (during_cycle - 1) / COLUMNS;
+        let during_cycle_zero_based = usize::try_from(during_cycle)
+            .with_context(|| format!("Provided during_cycle={during_cycle} is too big, because could not convert it from u128 to usize."))?
+            .checked_sub(1)
+            .ok_or_else(|| anyhow::anyhow!("Provided during_cycle={during_cycle} is probably 0, which is not allowed, because could not subtract 1 from it."))?;
+        let row = during_cycle_zero_based
+            .checked_div(COLUMNS)
+            .ok_or_else(|| {
+                anyhow::anyhow!(
+                    "Set COLUMNS={COLUMNS} might be 0, because could not divide with it."
+                )
+            })?;
         if row >= ROWS {
-            Err(anyhow::anyhow!(
-                "Received signal outside of row range (is: {row}, max: {ROWS})."
-            ))
+            Err(anyhow::anyhow!("Received signal with during_cycle={during_cycle} outside of row range (is: {row}, max: {ROWS})."))
         } else {
-            let column = isize::try_from((during_cycle - 1) % COLUMNS).unwrap();
-            if column + 1 < 0 || column - 1 >= isize::try_from(COLUMNS).unwrap() {
-                Err(anyhow::anyhow!(
-                    "Received signal outside of column range ({COLUMNS})."
-                ))
+            let column = during_cycle_zero_based
+                .checked_rem(COLUMNS)
+                .ok_or_else(|| anyhow::anyhow!("Set COLUMNS={COLUMNS} might be 0, because could not calculate a remainder with it."))?;
+            let column_signed = isize::try_from(column)
+                .with_context(|| format!("Provided during_cycle={during_cycle} is too big, because could not convert column={column} from usize into isize."))?;
+
+            let next_column = column_signed.checked_add(1)
+                .ok_or_else(|| anyhow::anyhow!("Provided during_cycle={during_cycle} is too big, because could not add 1 to column_signed={column_signed} ."))?;
+            let previous_column = column_signed.checked_sub(1)
+                .ok_or_else(|| anyhow::anyhow!("Provided during_cycle={during_cycle} is too small, because could not subtract 1 from column_signed={column_signed} ."))?;
+
+            let columns = isize::try_from(COLUMNS)
+                .with_context(|| format!("Set COLUMNS={COLUMNS} is too big, because could not convert COLUMNS={COLUMNS} from usize to isize."))?;
+
+            if next_column < 0 || previous_column >= columns {
+                Err(anyhow::anyhow!("Received signal outside of column range (is: {column_signed}, max: {COLUMNS})."))
             } else {
-                if column == signal as isize + 1 {
-                    self.buffer[row][column as usize] = Pixel::Lit;
-                } else if column == signal as isize {
-                    self.buffer[row][column as usize] = Pixel::Lit;
-                } else if column == signal as isize - 1 {
-                    self.buffer[row][column as usize] = Pixel::Lit;
+                let signal_signed = isize::try_from(signal)
+                    .with_context(|| format!("Provided signal={signal} is too big, because could not convert it from i64 to isize."))?;
+                let bigger_signal_signed = signal_signed.checked_add(1)
+                    .ok_or_else(|| anyhow::anyhow!("Provided signal={signal_signed} is too big, because could not add 1 to it."))?;
+                let smaller_signal_signed = signal_signed.checked_sub(1)
+                    .ok_or_else(|| anyhow::anyhow!("Provided signal={signal_signed} is too small, because could not subtract 1 from it."))?;
+
+                if column_signed == bigger_signal_signed
+                    || column_signed == signal_signed
+                    || column_signed == smaller_signal_signed
+                {
+                    *self.buffer
+                        .get_mut(row)
+                        .and_then(|buffer_row| buffer_row.get_mut(column))
+                        .ok_or_else(|| anyhow::anyhow!("Could not find pixel at column={column} and row={row}; might be an internal error."))? = Pixel::Lit;
                 }
                 Ok(())
             }
@@ -361,7 +398,7 @@ impl<const COLUMNS: usize, const ROWS: usize> CRT<COLUMNS, ROWS> {
     }
 }
 
-impl<const COLUMNS: usize, const ROWS: usize> Display for CRT<COLUMNS, ROWS> {
+impl<const COLUMNS: usize, const ROWS: usize> Display for Crt<COLUMNS, ROWS> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for (line, row) in self.buffer.iter().enumerate() {
             if line > 0 {
@@ -375,7 +412,7 @@ impl<const COLUMNS: usize, const ROWS: usize> Display for CRT<COLUMNS, ROWS> {
     }
 }
 
-impl<const COLUMNS: usize, const ROWS: usize> Default for CRT<COLUMNS, ROWS> {
+impl<const COLUMNS: usize, const ROWS: usize> Default for Crt<COLUMNS, ROWS> {
     fn default() -> Self {
         Self {
             buffer: [[Pixel::Dark; COLUMNS]; ROWS],
@@ -391,13 +428,15 @@ enum Pixel {
 
 impl Display for Pixel {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
+        match *self {
             Pixel::Lit => write!(f, "#"),
             Pixel::Dark => write!(f, "."),
         }
     }
 }
 
+#[allow(clippy::panic_in_result_fn)]
+#[allow(clippy::as_conversions)]
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -593,7 +632,7 @@ addx -5";
 
     #[test]
     fn test_cpu_without_clock_circuit() -> anyhow::Result<()> {
-        let mut cpu = CPU::new_with_register_x_value(1);
+        let mut cpu = Cpu::new_with_register_x_value(1);
         assert_eq!(cpu.x_register.value, 1);
 
         cpu.load(Program {
@@ -624,17 +663,17 @@ addx -5";
 
     #[test]
     fn test_cpu_with_clock_circuit() -> anyhow::Result<()> {
-        let mut cpu = CPU::new_with_register_x_value(1);
-        assert_eq!(cpu.x_register.value, 1);
+        let mut new_cpu = Cpu::new_with_register_x_value(1);
+        assert_eq!(new_cpu.x_register.value, 1);
 
-        cpu.load(Program {
+        new_cpu.load(Program {
             instructions: VecDeque::from(vec![Instruction::NoOp, Instruction::AddX(3)]),
         });
 
         let cycle_history = Rc::new(RefCell::new(Vec::new()));
         let cycle_history_cloned = Rc::clone(&cycle_history);
-        let mut clock_circuit = ClockCircuit::new(cpu);
-        clock_circuit.set_cycle_completed_callback(move |cpu: &CPU, cycles_completed: u128| {
+        let mut clock_circuit = ClockCircuit::new(new_cpu);
+        clock_circuit.set_cycle_completed_callback(move |cpu: &Cpu, cycles_completed: u128| {
             RefCell::borrow_mut(&cycle_history_cloned).push(cpu.x_register.value);
             assert_eq!(
                 cycles_completed,
