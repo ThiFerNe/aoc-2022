91c85217e70c668d4a7b21b4c4aa6a2058159cb8
ThiFerNe <thimo.f.neumann@gmail.com>
2022-12-29 16:22:47 +0100

    Add solution for day 13 part 2

--- src/day13.rs
+++ src/day13.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::fmt::{Display, Formatter};
 use std::str::FromStr;
 
@@ -7,6 +8,10 @@ fn main() {
     // Part 1 - 2 hours 36 minutes 58 seconds
     let part_1_solution = calculate_sum_of_indices_of_pairs_in_right_order(INPUT);
     println!("part_1_solution: {part_1_solution:?}");
+
+    // Part 2 - 21 minutes 56 seconds
+    let part_2_solution = calculate_decoder_key_for_distress_signal(INPUT);
+    println!("part_2_solution: {part_2_solution:?}");
 }
 
 fn calculate_sum_of_indices_of_pairs_in_right_order(input: &str) -> u64 {
@@ -18,6 +23,32 @@ fn calculate_sum_of_indices_of_pairs_in_right_order(input: &str) -> u64 {
         .filter(|(_, a)| a.is_in_right_order())
         .map(|(index, _)| index + 1)
         .sum::<usize>() as u64
+}
+
+fn calculate_decoder_key_for_distress_signal(input: &str) -> u64 {
+    let packet_pairs = PacketPairs::from_str(input).unwrap();
+    let mut m = packet_pairs
+        .0
+        .into_iter()
+        .flat_map(|k| vec![k.left, k.right])
+        .collect::<Vec<_>>();
+    let divider_packets = vec![
+        Packet(vec![PacketData::List(vec![PacketData::Integer(2)])]),
+        Packet(vec![PacketData::List(vec![PacketData::Integer(6)])]),
+    ];
+    m.extend_from_slice(divider_packets.as_slice());
+    m.sort_by(
+        |a, b| match list_of_packet_data_in_right_order(&a.0, &b.0) {
+            Order::Correct => std::cmp::Ordering::Less,
+            Order::Incorrect => std::cmp::Ordering::Greater,
+            Order::Indecisive => std::cmp::Ordering::Equal,
+        },
+    );
+    divider_packets
+        .iter()
+        .map(|divider_packet| m.iter().find_position(|a| *a == divider_packet).unwrap().0)
+        .map(|k| k + 1)
+        .product::<usize>() as u64
 }
 
 #[derive(Debug, Clone)]
@@ -41,54 +72,52 @@ struct PacketPair {
     right: Packet,
 }
 
-impl PacketPair {
-    fn is_in_right_order(&self) -> bool {
-        enum Order {
-            Correct,
-            Incorrect,
-            Indecisive,
-        }
+enum Order {
+    Correct,
+    Incorrect,
+    Indecisive,
+}
 
-        fn list_of_packet_data_in_right_order(left: &[PacketData], right: &[PacketData]) -> Order {
-            for index in 0..(left.len().min(right.len())) {
-                match packet_data_in_right_order(&left[index], &right[index]) {
-                    Order::Correct => return Order::Correct,
-                    Order::Incorrect => return Order::Incorrect,
-                    Order::Indecisive => {}
-                }
-            }
-            if left.len() < right.len() {
+fn list_of_packet_data_in_right_order(left: &[PacketData], right: &[PacketData]) -> Order {
+    for index in 0..(left.len().min(right.len())) {
+        match packet_data_in_right_order(&left[index], &right[index]) {
+            Order::Correct => return Order::Correct,
+            Order::Incorrect => return Order::Incorrect,
+            Order::Indecisive => {}
+        }
+    }
+    if left.len() < right.len() {
+        Order::Correct
+    } else if left.len() > right.len() {
+        Order::Incorrect
+    } else {
+        Order::Indecisive
+    }
+}
+
+fn packet_data_in_right_order(left: &PacketData, right: &PacketData) -> Order {
+    match (left, right) {
+        (PacketData::Integer(a), PacketData::Integer(b)) => {
+            if *a < *b {
                 Order::Correct
-            } else if left.len() > right.len() {
+            } else if *a > *b {
                 Order::Incorrect
             } else {
                 Order::Indecisive
             }
         }
-
-        fn packet_data_in_right_order(left: &PacketData, right: &PacketData) -> Order {
-            match (left, right) {
-                (PacketData::Integer(a), PacketData::Integer(b)) => {
-                    if *a < *b {
-                        Order::Correct
-                    } else if *a > *b {
-                        Order::Incorrect
-                    } else {
-                        Order::Indecisive
-                    }
-                }
-                (PacketData::Integer(a), PacketData::List(b)) => {
-                    list_of_packet_data_in_right_order(&[PacketData::Integer(*a)], &b)
-                }
-                (PacketData::List(a), PacketData::Integer(b)) => {
-                    list_of_packet_data_in_right_order(&a, &[PacketData::Integer(*b)])
-                }
-                (PacketData::List(a), PacketData::List(b)) => {
-                    list_of_packet_data_in_right_order(&a, &b)
-                }
-            }
+        (PacketData::Integer(a), PacketData::List(b)) => {
+            list_of_packet_data_in_right_order(&[PacketData::Integer(*a)], &b)
         }
+        (PacketData::List(a), PacketData::Integer(b)) => {
+            list_of_packet_data_in_right_order(&a, &[PacketData::Integer(*b)])
+        }
+        (PacketData::List(a), PacketData::List(b)) => list_of_packet_data_in_right_order(&a, &b),
+    }
+}
 
+impl PacketPair {
+    fn is_in_right_order(&self) -> bool {
         matches!(
             list_of_packet_data_in_right_order(&self.left.0, &self.right.0),
             Order::Correct
@@ -113,7 +142,7 @@ impl FromStr for PacketPair {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 struct Packet(Vec<PacketData>);
 
 impl FromStr for Packet {
@@ -141,7 +170,7 @@ impl Display for Packet {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 enum PacketData {
     List(Vec<PacketData>),
     Integer(u8),
@@ -274,5 +303,14 @@ mod tests {
 
         // Assert
         assert_eq!(result, 13);
+    }
+
+    #[test]
+    fn test_part_2_default() {
+        // Act
+        let result = calculate_decoder_key_for_distress_signal(TEST_INPUT);
+
+        // Assert
+        assert_eq!(result, 140);
     }
 }
